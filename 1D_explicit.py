3a430b48e575891c67ff70f1d398a1aada2efafd
ry0u <ry0u_yd@yahoo.co.jp>
2016-11-04 00:11:41 +0900

    update parameter

--- 1D_explicit.py
+++ 1D_explicit.py
@@ -3,37 +3,44 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import animation
 
-STEP = 100
-delta_t = 0.1
-delta_x = 1.0
-k = 1.0
-S = [0.0, 0.0, 0.0, 0.0]
-K = k * delta_t / (delta_x * delta_x)
-title = 'STEP:' + str(STEP) + ' delta_t:' + str(delta_t) + ' delta_x:' + str(delta_x) + ' k:' + str(k)
-
+# -------------- parameter ----------------
 n = 4
-mat = [[0.0, 0.0, 0.0, 0.0], [1.0, -2.0, 1.0, 0.0], [0.0, 1.0, -2.0, 1.0], [0.0, 0.0, 0.0, 0.0]]
-t = [0.0, 0.0, 0.0, 100.0]
+STEP = 100
+dt = 0.1
+dx = 1.0
+k = 1.0
+K = k * dt / (dx * dx)
+
+S = np.zeros(n)
+
+T = np.zeros(n)
+T[n-1] = 100
+
+# -------------- matrix ----------------
+mat = np.zeros([n, n])
+for i in range(1, n-1):
+    mat[i][i-1] = 1
+    mat[i][i] = -2
+    mat[i][i+1] = 1
 
 fig = plt.figure()
-ims = [plt.plot(t, 'r')]
-
-line = [t]
+ims = [plt.plot(T, 'r')]
 
 for i in range(STEP):
-    next = copy.deepcopy(t)
+    next = copy.deepcopy(T)
 
     for j in range(n):
         res = 0.0
         for k in range(n):
-            res = res + mat[j][k] * t[k]
-        next[j] = next[j] + res * K + S[j] * delta_t
-    t = copy.deepcopy(next)
+            res = res + mat[j][k] * T[k]
+        next[j] = next[j] + res * K + S[j] * dt
+    T = copy.deepcopy(next)
+    ims.append(plt.plot(T, 'r'))
 
-    ims.append(plt.plot(t, 'r'))
-
-plt.title(title)
+plt.title('1D - explicit')
 plt.ylabel('T')
 plt.xlabel('x')
 ani = animation.ArtistAnimation(fig, ims)
 # ani.save("n4_S4.gif", writer="imagemagick")
+
+plt.show()
